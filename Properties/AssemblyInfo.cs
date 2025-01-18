41b566297e389932373e146ed36d0149260d7a49
Vinicius Hissayoshi Nishida <viniciusnishida13@gmail.com>
2025-01-18 12:00:25 -0300

    refactor: update AssemblyInfo.cs with improved title, description, and versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("notSoRealistic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Not So Realistic")]
+[assembly: AssemblyDescription("BepInEx plugin for EFT that improves medical kits and adds quality of life features.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("notSoRealistic")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyProduct("Not So Realistic")]
+[assembly: AssemblyCopyright("Copyright © Vinicius Hissayoshi Nishida (viniHNS) 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
